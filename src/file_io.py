2b5b9bc17136f2048641f72a29c21e4f17bd63e8
Trevor Fehrman <trevorfehrman@gmail.com>
2019-04-19 23:36:06 -0700

    file_io

--- src/file_io.py
+++ src/file_io.py
@@ -10,9 +10,24 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 
 # YOUR CODE HERE
 
+with open ('foo.txt') as f:
+    read_data = f.read()
+    print(read_data)
+
+
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make 
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+
+with open ('bar.txt', 'w') as f:
+    f.write("Early one mornin' the sun was shinin'\n")
+    f.write("She was layin in bed\n")
+    f.write("Wonderin if she had changed at all\n")
+    f.write("If her hair was still red\n")
+
+with open ('bar.txt') as f:
+    read_data = f.read()
+    print(read_data)
